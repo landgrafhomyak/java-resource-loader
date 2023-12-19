8dad64d49bdaa083b3f679a875f452062c5c3447
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2023-12-19 20:05:43 +0300

    Updating kotlin functions

--- src/main/kotlin/ru/landgrafhomyak/utility/JavaResourceLoader.kt
+++ src/main/kotlin/ru/landgrafhomyak/utility/JavaResourceLoader.kt
@@ -2,53 +2,57 @@
 
 package ru.landgrafhomyak.utility
 
+import java.lang.Class
 import java.io.IOException
 import java.nio.charset.Charset
 
 @Throws(IOException::class)
-fun ClassLoader.getResourceAsBytes(path: String): ByteArray =
-    JavaResourceLoader.loadBytes(this, path)
+fun ClassLoader.getResourceAsBytesFromRoot(path: String): ByteArray =
+    JavaResourceLoader.loadBytesFromRoot(this, path)
 
 @Throws(IOException::class)
-fun Class<*>.getResourceAsBytes(path: String): ByteArray =
-    JavaResourceLoader.loadBytes(this, path)
-
-fun ClassLoader.getResourceAsBytesExitOnFail(
-    path: String,
-    exitCode: Int = JavaResourceLoader.DEFAULT_EXIT_CODE
-): ByteArray = JavaResourceLoader.loadBytesExitOnFail(this, path, exitCode)
-
-fun Class<*>.getResourceAsBytesExitOnFail(
-    path: String,
-    exitCode: Int = JavaResourceLoader.DEFAULT_EXIT_CODE
-): ByteArray = JavaResourceLoader.loadBytesExitOnFail(this, path, exitCode)
+fun Class<*>.getResourceAsBytesFromRoot(path: String): ByteArray =
+    JavaResourceLoader.loadBytesFromRoot(this, path)
 
 @Throws(IOException::class)
-fun ClassLoader.getResourceAsText(path: String, charset: Charset = JavaResourceLoader.DEFAULT_CHARSET): String =
-    JavaResourceLoader.loadText(this, path, charset)
+fun Class<*>.getResourceAsBytesRelative(path: String): ByteArray =
+    JavaResourceLoader.loadBytesRelative(this, path)
+
+fun ClassLoader.getResourceAsBytesFromRootExitOnFail(path: String, exitCode: Int = JavaResourceLoader.DEFAULT_EXIT_CODE): ByteArray =
+    JavaResourceLoader.loadBytesFromRootExitOnFail(this, path, exitCode)
+
+fun Class<*>.getResourceAsBytesFromRootExitOnFail(path: String, exitCode: Int = JavaResourceLoader.DEFAULT_EXIT_CODE): ByteArray =
+    JavaResourceLoader.loadBytesFromRootExitOnFail(this, path, exitCode)
+
+fun Class<*>.getResourceAsBytesRelativeExitOnFail(path: String, exitCode: Int = JavaResourceLoader.DEFAULT_EXIT_CODE): ByteArray =
+    JavaResourceLoader.loadBytesRelativeExitOnFail(this, path, exitCode)
 
 @Throws(IOException::class)
-fun Class<*>.getResourceAsText(path: String, charset: Charset = JavaResourceLoader.DEFAULT_CHARSET): String =
-    JavaResourceLoader.loadText(this, path, charset)
+fun ClassLoader.getResourceAsTextFromRoot(path: String, charset: Charset = JavaResourceLoader.DEFAULT_CHARSET): String =
+    JavaResourceLoader.loadTextFromRoot(this, path, charset)
 
-fun ClassLoader.getResourceAsTextExitOnFail(
-    path: String,
-    charset: Charset = JavaResourceLoader.DEFAULT_CHARSET,
-    exitCode: Int = JavaResourceLoader.DEFAULT_EXIT_CODE
-): String = JavaResourceLoader.loadTextExitOnFail(this, path, charset, exitCode)
+@Throws(IOException::class)
+fun Class<*>.getResourceAsTextFromRoot(path: String, charset: Charset = JavaResourceLoader.DEFAULT_CHARSET): String =
+    JavaResourceLoader.loadTextFromRoot(this, path, charset)
 
-fun ClassLoader.getResourceAsTextExitOnFail(
-    path: String,
-    exitCode: Int
-): String = JavaResourceLoader.loadTextExitOnFail(this, path, exitCode)
+@Throws(IOException::class)
+fun Class<*>.getResourceAsTextRelative(path: String, charset: Charset = JavaResourceLoader.DEFAULT_CHARSET): String =
+    JavaResourceLoader.loadTextRelative(this, path, charset)
 
-fun Class<*>.getResourceAsTextExitOnFail(
-    path: String,
-    charset: Charset = JavaResourceLoader.DEFAULT_CHARSET,
-    exitCode: Int = JavaResourceLoader.DEFAULT_EXIT_CODE
-): String = JavaResourceLoader.loadTextExitOnFail(this, path, charset, exitCode)
+fun ClassLoader.getResourceAsTextFromRootExitOnFail(path: String, charset: Charset = JavaResourceLoader.DEFAULT_CHARSET, exitCode: Int = JavaResourceLoader.DEFAULT_EXIT_CODE): String =
+    JavaResourceLoader.loadTextFromRootExitOnFail(this, path, charset, exitCode)
 
-fun Class<*>.getResourceAsTextExitOnFail(
-    path: String,
-    exitCode: Int
-): String = JavaResourceLoader.loadTextExitOnFail(this, path, exitCode)
+fun Class<*>.getResourceAsTextFromRootExitOnFail(path: String, charset: Charset = JavaResourceLoader.DEFAULT_CHARSET, exitCode: Int = JavaResourceLoader.DEFAULT_EXIT_CODE): String =
+    JavaResourceLoader.loadTextFromRootExitOnFail(this, path, charset, exitCode)
+
+fun Class<*>.getResourceAsTextRelativeExitOnFail(path: String, charset: Charset = JavaResourceLoader.DEFAULT_CHARSET, exitCode: Int = JavaResourceLoader.DEFAULT_EXIT_CODE): String =
+    JavaResourceLoader.loadTextRelativeExitOnFail(this, path, charset, exitCode)
+
+fun ClassLoader.getResourceAsTextFromRootExitOnFail(path: String, exitCode: Int): String =
+    JavaResourceLoader.loadTextFromRootExitOnFail(this, path, JavaResourceLoader.DEFAULT_CHARSET, exitCode)
+
+fun Class<*>.getResourceAsTextFromRootExitOnFail(path: String, exitCode: Int): String =
+    JavaResourceLoader.loadTextFromRootExitOnFail(this, path, JavaResourceLoader.DEFAULT_CHARSET, exitCode)
+
+fun Class<*>.getResourceAsTextRelativeExitOnFail(path: String, exitCode: Int): String =
+    JavaResourceLoader.loadTextRelativeExitOnFail(this, path, JavaResourceLoader.DEFAULT_CHARSET, exitCode)
